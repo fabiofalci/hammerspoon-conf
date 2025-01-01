8e7dcfc89b4114ec048d98de115d195854896a11
Fabio Falci <fabiofalci@gmail.com>
2025-01-01 19:33:20 +0000

    Add caffeine menubar

--- init.lua
+++ init.lua
@@ -5,6 +5,24 @@ require("raycast")
 ---@diagnostic disable-next-line: undefined-global
 local hs = hs
 
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+	if state then
+		caffeine:setTitle("A")
+	else
+		caffeine:setTitle("S")
+	end
+end
+
+function caffeineClicked()
+	setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+	caffeine:setClickCallback(caffeineClicked)
+	setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
 -- Reload confirmation
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
 	hs.reload()
