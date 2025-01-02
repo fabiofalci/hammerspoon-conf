beca70581bd4bfa11630e35f5a249d0e6c43f06c
Fabio Falci <fabiofalci@gmail.com>
2025-01-02 20:53:32 +0000

    Fix full screen with gap test

--- window_manager_functions.lua
+++ window_manager_functions.lua
@@ -6,12 +6,12 @@ local hs = hs
 local outterGap = 10
 local innerGap = 10
 
-local outterDoubleGap = outterGap * 2
+local doubleOutterGap = outterGap * 2
 
 function window_manager_functions.setGaps(oGap, iGap)
 	outterGap = oGap
 	innerGap = iGap
-	outterDoubleGap = outterGap * 2
+	doubleOutterGap = outterGap * 2
 end
 
 function window_manager_functions.mmaximize()
@@ -37,8 +37,8 @@ function window_manager_functions.maximize()
 
 	f.x = max.x + outterGap
 	f.y = max.y + outterGap
-	f.w = max.w - outterDoubleGap
-	f.h = max.h - outterDoubleGap
+	f.w = max.w - doubleOutterGap
+	f.h = max.h - doubleOutterGap
 	win:setFrame(f)
 
 	return f
