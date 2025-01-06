7722c3e4a43c2d61cb28f0b064a2c86547746a1f
Fabio Falci <fabiofalci@gmail.com>
2025-01-06 17:53:27 +0000

    Trying a simple way of testing

---
+++ Spoons/SimpleWindowManager.spoon/mock_hs.lua
@@ -0,0 +1,34 @@
+---@diagnostic disable: unused-local
+hs = {
+	window = {
+
+		focusedWindow = function()
+			return {
+				setFrame = function(self, frame)
+					hs.window.lastFrame = frame
+				end,
+				frame = function(self)
+					return {
+						x = 0,
+						y = 0,
+						w = 0,
+						h = 0,
+					}
+				end,
+				screen = function(self)
+					return {
+						frame = function(s)
+							return {
+								x = 0,
+								y = 0,
+								w = 300,
+								h = 351,
+							}
+						end,
+					}
+				end,
+			}
+		end,
+		lastFrame = nil,
+	},
+}
